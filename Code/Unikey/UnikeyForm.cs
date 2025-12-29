c01a5ecd9c6f2f4052fed869f24fa8d06d4ebdbf
Stanislav <stanislav@pikkatech.eu>
2025-12-29 12:28:58 +0100

    updated UnikeyForm

--- Code/Unikey/UnikeyForm.cs
+++ Code/Unikey/UnikeyForm.cs
@@ -3,9 +3,17 @@ namespace Unikey
 {
 	public partial class UnikeyForm : Form
 	{
+		private NotifyIcon trayIcon;
+		private ContextMenuStrip trayMenu;
+
 		public UnikeyForm()
 		{
 			InitializeComponent();
+
+			this.ShowInTaskbar = false;
+			this.WindowState = FormWindowState.Normal;
+
+			this.InitializeTray();
 
 			string fileName = "C:\\pikkatech.eu\\Projects\\Active\\Unikey\\Images\\Raster\\ibisana++.jpg";
 
@@ -33,11 +41,82 @@ namespace Unikey
 
 			int x = wa.Left + (wa.Width - this.Width) / 2;
 
-			int margin = 0;	// -> settings
+			int margin = 0; // -> settings
 			int y = wa.Bottom - this.Height - margin;
 
 			this.StartPosition = FormStartPosition.Manual;
 			this.Location = new Point(x, y);
 		}
+
+		protected override void OnFormClosing(FormClosingEventArgs e)
+		{
+			e.Cancel = true;
+			this.Hide();
+		}
+
+
+		private void InitializeTray()
+		{
+			trayMenu = new ContextMenuStrip();
+
+			var toggleItem = new ToolStripMenuItem("Hide Display");
+			toggleItem.Click += (s, e) => ToggleDisplay(toggleItem);
+
+			var settingsItem = new ToolStripMenuItem("Settings");
+			settingsItem.Click += (s, e) => ShowSettings();
+
+			var exitItem = new ToolStripMenuItem("Quit");
+			exitItem.Click += (s, e) => ExitApplication();
+
+			trayMenu.Items.Add(toggleItem);
+			trayMenu.Items.Add(new ToolStripSeparator());
+			trayMenu.Items.Add(settingsItem);
+			trayMenu.Items.Add(new ToolStripSeparator());
+			trayMenu.Items.Add(exitItem);
+
+			trayIcon = new NotifyIcon
+			{
+				Text = "My Semi-Transparent Overlay",
+				Icon = this.Icon, // or load a custom .ico
+				ContextMenuStrip = trayMenu,
+				Visible = true
+			};
+
+			trayIcon.DoubleClick += (s, e) => ToggleDisplay(toggleItem);
+		}
+
+		private void ToggleDisplay(ToolStripMenuItem menuItem)
+		{
+			if (this.Visible)
+			{
+				this.Hide();
+				menuItem.Text = "Show Display";
+			}
+			else
+			{
+				this.Show();
+				this.Activate();
+				menuItem.Text = "Hide Display";
+			}
+		}
+
+
+		private void ShowSettings()
+		{
+			MessageBox.Show(
+				"Settings dialog goes here.",
+				"Settings",
+				MessageBoxButtons.OK,
+				MessageBoxIcon.Information);
+		}
+
+
+		private void ExitApplication()
+		{
+			trayIcon.Visible = false;
+			trayIcon.Dispose();
+			Application.Exit();
+		}
+
 	}
 }
